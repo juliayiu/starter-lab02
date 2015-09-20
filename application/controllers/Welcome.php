a9cb81b150291805908865b6c771398d01e11e8b
Julia <jyiu93@gmail.com>
2015-09-20 13:14:38 -0700

    convert from basic to good codigniter (tutorial 3)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,6 +8,27 @@ class Welcome extends Application {
      * Index Page for this controller.
      */
     public function index() {
+        // get the newest images for our model
+        $pix = $this->images->newest();
+
+        // build an array of formatted cells for them
+        foreach ($pix as $picture)
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+
+        // prime the table class
+        $this->load->library('table');
+        $parms = array(
+            'table_open' => '<table class = "gallery">',
+            'cell_start' => '<td class = "oneimage">',
+            'cell_alt_start' => '<td class = "oneimage">'
+        );
+        $this->table->set_template($parms);
+
+        // finally! Generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
+
+        // render
         $this->data['pagebody'] = 'welcome';
         $this->render();
     }
